e89ea9efd4ec72cdc0775af6b391679ac4d7d9df
uwaechte <uwaechte@0e4a7186-e024-0410-a49b-aced43e0d94e>
2009-01-31 13:51:56 +0000

    more cleanups and ubuntu/intrepid fixes
    git-svn-id: https://svn.ikw.uni-osnabrueck.de/dav/ikwadmin/linux/puppetmaster/etc_puppet/modules/nagios@2123 0e4a7186-e024-0410-a49b-aced43e0d94e

--- manifests/defines/nagios_command.pp
+++ manifests/defines/nagios_command.pp
@@ -1,64 +1,66 @@
 #$Id$
 
 define nagios2_command (
-		$command_name="",
-		$command_line,
-		$ensure = "present"
-		)
+    $command_name="",
+    $command_line,
+    $ensure = "present"
+    )
 {
-	$cmd_real = $command_name ? {
-		"" => $name,
-		default => $command_name,
-	}
-	nagios2file { "command_${cmd_real}":
-		content => template("nagios/command.erb"),
-		ensure =>$ensure,
-	}
+  $cmd_real = $command_name ? {
+    "" => $name,
+    default => $command_name,
+  }
+  nagios2file { "command_${cmd_real}":
+    content => template("nagios/command.erb"),
+    ensure =>$ensure,
+  }
 
 }
 
 define nagios2_nrpe_command (
-		$command_name="",
-		$command_line,
-		$ensure = "present",
-		$sudo = false
-		)
+    $command_name="",
+    $command_line,
+    $ensure = "present",
+    $sudo = false
+    )
 {
-	$cmd_real = $command_name ? {
-		"" => $name,
-		default => $command_name,
-	}
-	$sudobin = $kernel ? {
-	  "FreeBSD" => "/usr/local/bin/sudo",
-	  default => "/usr/bin/sudo",
-	}
-	case $sudo {
-		true: {
-			sudo::sudoer{"nagios_sudo_${hostname}_${cmd_real}":
-				user => "nagios",
-				host_name => $hostname,
-				command => "NOPASSWD: ${command_line}",
-				ensure => $ensure,
-			}
-			$command_line_real = "${sudobin} ${command_line}"
-		}
-		false:{
-			$command_line_real = $command_line
-		}
-	}
-	$nagioscfg = $operatingsystem ? {
-	  "FreeBSD" => "/usr/local/etc/nrpe_local.cfg",
-	    default =>"/etc/nagios/nrpe_local.cfg",
-	}
-	if ($command_line_real != ""){
-	line { "nagios_nrpe_${name}":
-	  file => "${nagioscfg}",
-	       line => "command[${cmd_real}]=${command_line_real}",
-	       ensure => $ensure,
-	}
-	}
-	
-	exec{"sed -ie '/^command\[.*\]=$/d' ${nagioscfg}":
-	 onlyif => "qrep -qe '^command\[.*\]=$'",  
+  $cmd_real = $command_name ? {
+    "" => $name,
+    default => $command_name,
+  }
+  $sudobin = $kernel ? {
+    "FreeBSD" => "/usr/local/bin/sudo",
+    default => "/usr/bin/sudo",
+  }
+  case $sudo {
+    "true": {
+      sudo::sudoer{"nagios_sudo_${hostname}_${cmd_real}":
+	user => "nagios",
+	host_name => $hostname,
+	command => "NOPASSWD: ${command_line}",
+	ensure => $ensure,
+      }
+      $command_line_real = "${sudobin} ${command_line}"
+    }
+    "false":{
+      $command_line_real = $command_line
+    }
+  }
+  $nagioscfg = $operatingsystem ? {
+    "FreeBSD" => "/usr/local/etc/nrpe_local.cfg",
+      default =>"/etc/nagios/nrpe_local.cfg",
+  }
+  if ($command_line_real != ""){
+    line { "nagios_nrpe_${name}":
+      file => "${nagioscfg}",
+	   line => "command[${cmd_real}]=${command_line_real}",
+	   ensure => $ensure,
+    }
+  }
+  if defined(Exec["cleanup_nrpe_local"]) != true {
+    exec{"cleanup_nrpe_local":
+      command => "/bin/sed -ie '/^command\\[.*\\]=$/d' ${nagioscfg}",
+	      onlyif => "/bin/grep -qe '^command\\[.*\\]=$'",  
+    }
+  }
 }
-	}
