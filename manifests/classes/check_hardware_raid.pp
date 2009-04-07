cf68372374f434606826770f74280d33a59ec601
uwaechte <uwaechte@0e4a7186-e024-0410-a49b-aced43e0d94e>
2009-04-07 13:00:09 +0000

    removing raid check for data
    git-svn-id: https://svn.ikw.uni-osnabrueck.de/dav/ikwadmin/linux/puppetmaster/etc_puppet/modules/nagios@2379 0e4a7186-e024-0410-a49b-aced43e0d94e

--- manifests/classes/check_hardware_raid.pp
+++ manifests/classes/check_hardware_raid.pp
@@ -9,11 +9,20 @@ class nagios::check::raid::software {
 }
 
 class nagios::check::raid::three_ware {
+  $prese_real = $presence ? { 
+    "absent" => "absent",
+      default => "present"
+  }
   nagios2_nrpe_plugin {"${fqdn}_checkraid":
     service_description => "CHECKRAID",
 			command_name => "check_3ware_raid",
 			notification_options => "w,c,u",
 			sudo => true,
+			ensure => $prese_real,
   }
+}
+class nagios::check::raid::three_ware::none {
+  $presence = "absent"
+    include nagios::check::raid::three_ware
 }
 
