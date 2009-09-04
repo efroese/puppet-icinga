43e1d5901112454bb18910ea8f07049ad8521fe8
uwaechte <uwaechte@0e4a7186-e024-0410-a49b-aced43e0d94e>
2009-09-04 17:24:39 +0000

    git-svn-id: https://svn.ikw.uni-osnabrueck.de/dav/ikwadmin/linux/puppetmaster/etc_puppet/modules/nagios@2826 0e4a7186-e024-0410-a49b-aced43e0d94e

--- manifests/classes/check_hardware_raid.pp
+++ manifests/classes/check_hardware_raid.pp
@@ -2,31 +2,31 @@
 
 class nagios::check::raid::software {
   $prese_real = $presence ? { 
-      "absent" => "absent",
-        default => $has_raid ? {
-          "false" => "absent",
-            default => "present"
-        }
-    }
+    "absent" => "absent",
+      default => $has_raid ? {
+	"false" => "absent",
+	default => "present"
+      }
+  }
   nagios2_nrpe_plugin {"${fqdn}_checkraid":
     service_description => "CHECKRAID",
 			command_name => "check_raid",
 			servicegroups => "Harddrives",
 			notification_options => "w,c,u",
-    ensure => $prese_real,
+			ensure => $prese_real,
   }
 }
-  class nagios::check::raid::software::none {
-    $presence = "absent"
+class nagios::check::raid::software::none {
+  $presence = "absent"
     include nagios::check::raid::software
-  }
+}
 
 class nagios::check::raid::three_ware {
   $prese_real = $presence ? { 
     "absent" => "absent",
       default => $has_raid ? {
-        "false" => "absent",
-          default => "present"
+	"false" => "absent",
+	default => "present"
       }
   }
   nagios2_nrpe_plugin {"${fqdn}_checkraid":
