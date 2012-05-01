b9aeb163508d8e8762cb82fbce3b2e438e3e7ca0
Erik Froese <erik.froese@gmail.com>
2012-05-01 09:59:56 -0700

    Use the proper syntax for a rpm fetched via http.

--- manifests/repos.pp
+++ manifests/repos.pp
@@ -1,8 +1,9 @@
 class icinga::repos {
     case $::operatingsystem {
         RedHat,CentOS,Amazon : {
-            package { 'http://pkgs.repoforge.org/rpmforge-release/rpmforge-release-0.5.2-2.el6.rf.i686.rpm':
+            package { "rpmforge-release-0.5.2-2.el6.rf.${::architecture}":
                 ensure => installed,
+                source => "http://pkgs.repoforge.org/rpmforge-release/rpmforge-release-0.5.2-2.el6.rf.${::architecture}.rpm",
                 provider => 'rpm',
             }
         }
