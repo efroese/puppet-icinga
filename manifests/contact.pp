42edcd12d570722f090e174512187ad2be6b15a3
Erik Froese <erik.froese@gmail.com>
2012-05-03 15:49:54 -0400

    Split contactgroup into its own file.

--- manifests/contact.pp
+++ manifests/contact.pp
@@ -39,26 +39,3 @@ define icinga::contact (
   }
 
 }
-
-#define contactgroup{
-# contactgroup_name contactgroup_name
-#  alias alias
-# members members
-# }
-
-define icinga::contactgroup (
-    $contactgroup_name="",
-    $contactgroup_alias,
-    $members,
-    $ensure = "present"
-    )
-{
-  $contactgroup_name_real = $contactgroup_name ?{
-    "" => $name,
-    default => $contactgroup_name
-  }
-   icinga::object { "contactgroup_${contactgroup_name_real}":
-    content => template("icinga/contactgroup.erb"),
-    ensure =>$ensure,
-  }
-}
