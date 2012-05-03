e0f916de896cba5a62f572176eb1ff1b006dc223
Erik Froese <erik.froese@gmail.com>
2012-05-03 15:44:35 -0400

    No passing params to the params.pp

--- manifests/params.pp
+++ manifests/params.pp
@@ -2,23 +2,12 @@
 # = Class icinga::params
 # Holds config variables for icinga
 #
-# == Parameters
-#
-# $nagios_conf_dir:: Where to store the generated icinga/nagios objects.
-#
-# $nagiosplugins:: The path to the nagios plugins
-#
-class icinga::params (
-    $nagios_conf_dir = "/etc/icinga/objects.d",
-    $nagiosplugins = ''
-    ) {
+class icinga::params {
+    
+    $nagios_conf_dir = '/etc/icings/objects.d'
 
-    $nagiosplugins = $nagiosplugins ? {
-        '' =>  $architecture ? {
-            x86_64  => '/usr/lib64/nagios/plugins',
-            default => '/usr/lib/nagios/plugins',
-        },
-        default => $nagiosplugins,
+    $nagiosplugins = $architecture ? {
+        x86_64  => '/usr/lib64/nagios/plugins',
+        default => '/usr/lib/nagios/plugins',
     }
-
 }
