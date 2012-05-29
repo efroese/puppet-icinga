f04686bd1a953851a1b9fcc6993ddd3466b00ade
Erik Froese <erik.froese@gmail.com>
2012-05-29 15:25:31 -0400

    Add submit_host_result command.

--- manifests/nsca/submit.pp
+++ manifests/nsca/submit.pp
@@ -1,6 +1,8 @@
 #
 # = Class icinga::nsca::submit
-# Define the submit_check_result icinga command for NSCA senders.
+# Define the submit_check_result and cubmit_host_result icinga commands
+# for NSCA senders.
+#
 # Only ONE node should declare this class. All distributed icinga
 # servers should collect this check as part of collecting all of 
 # the icinga::command resources
@@ -12,5 +14,10 @@ class icinga::nsca::submit {
     icinga::command { 'submit_check_result':
         ensure       => present,
         command_line => "${icinga::params::eventhandlers}/submit_check_result \$HOSTNAME\$ '\$SERVICEDESC\$' \$SERVICESTATE\$ '\$SERVICEOUTPUT\$'",
+    }
+
+    icinga::command { 'submit_host_result':
+        ensure       => present,
+        command_line => "${icinga::params::eventhandlers}/submit_host_result \$HOSTNAME\$ \$HOSTSTATEID\$ '\$HOSTOUTPUT\$'",
     }
 }
